def4a95d833c8df2c190c28ffe3834fbb2fb85e1
Ajith8Elonnative <ajithkumar@elonnativesystem.com>
2024-11-04 17:06:24 +0530

    chngs

--- Swagger.js
+++ Swagger.js
@@ -5,7 +5,7 @@ const doc = {
     title: "User Crud API's",
     description: "Version 2.0"
   },
-  host: "https://signup-and-login.onrender.com",
+  host: "http://signup-and-login.onrender.com",
   basePath:"/",
 };
 
