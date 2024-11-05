73b3e71534ee70e7511ef9192a50100808d415ec
Ajith8Elonnative <ajithkumar@elonnativesystem.com>
2024-11-05 10:09:55 +0530

    bug fix1

--- main.js
+++ main.js
@@ -2,24 +2,25 @@ const express = require('express')
 const employeeDetailsRoutes = require('./routes/employeeDetails.route.js')
 const customerDetailsRoutes = require('./routes/customerDetails.route.js')
 const signupRoutes = require('./routes/signup.route.js')
-const mongoose =require("mongoose");
-const dotenv= require('dotenv')
+const mongoose = require("mongoose");
+const dotenv = require('dotenv')
 const cors = require('cors')
 const swaggerUi = require('swagger-ui-express')
-const swaggerDocument = require('./swagger-output.json') 
+const swaggerDocument = require('./swagger-output.json')
 const bodyparser = require('body-parser')
 
 const app = express();
 const PORT = 4000
 
-app.use(cors())
+app.use(cors({
+    origin:"https://signup-and-login.onrender.com"
+}))
 app.use(bodyparser.json())
 dotenv.config();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }))
 
-// app.use('/docs', swaggerDoc.serve)
-// app.use('/docs', swaggerDoc.setup(swaggerDocumentation))
+
 app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
 app.get('/', (req, res) => {
